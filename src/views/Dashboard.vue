1e375b60993c7f1172f15b385ad19e2d70144b9a
Ray Bednara <ray.bednara@att.net>
2019-04-18 17:22:25 -0500

    update dashboard to show logged in user

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,22 @@
 <template>
   <div>This is dashboard page.</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  },
+  created() {
+    try {
+      console.log("Logged in.");
+      const user = JSON.parse(localStorage.getItem("classroom.user"));
+      console.log(user);
+    } catch (er) {
+      console.error(er);
+    }
+  }
+};
+</script>
